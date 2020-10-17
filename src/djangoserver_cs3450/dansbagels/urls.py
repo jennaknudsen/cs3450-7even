eb5812cd5fa7d2fad3ba40e0dedb5286a0d1c4dd
Kaden <kdex95@yahoo.com>
2020-10-16 21:03:41 -0600

    I created a branch called proto-kaden in which I will create a prototype that dispays database info

--- src/djangoserver_cs3450/dansbagels/urls.py
+++ src/djangoserver_cs3450/dansbagels/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('test/', views.test, name='test'),
+    path('prototype2', views.prototype2, name='prototype2'),
 ]
--- src/djangoserver_cs3450/dansbagels/views.py
+++ src/djangoserver_cs3450/dansbagels/views.py
@@ -3,8 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
     return HttpResponse("Hello, world. You're at the dansbagels index.")
 
+
 def test(request):
     return HttpResponse("You made it to the dansbagels test.")
+
+
+def prototype2(request):
+    return HttpResponse("This is where I will put the first prototype.")
