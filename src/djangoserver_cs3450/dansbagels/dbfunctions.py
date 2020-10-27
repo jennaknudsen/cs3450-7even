986f98f41467d04224648fdf55554984a636f70c
Jonas Knudsen <jonas.a.knudsen@gmail.com>
2020-10-27 10:31:56 -0600

    Added createAccount() function, set up unit testing

--- src/djangoserver_cs3450/dansbagels/dbfunctions.py
+++ src/djangoserver_cs3450/dansbagels/dbfunctions.py
@@ -1,7 +1,10 @@
 from dansbagels.models import *
 
-# Helper function to return true if the username/password combination is 
-# in the database, false otherwise.
+# Change this variable to True if you want debug messages to print 
+printDebugMessages = True
+
+# Check to see if a given username/combination is valid.
+# Return true if the username/password combination is in the database, false otherwise.
 def verifyLogin(username, password):
     listOfUsers = []
     allPeople = Person.objects.all()
@@ -10,4 +13,32 @@ def verifyLogin(username, password):
     return (username, password) in listOfUsers
 
 
+# Create an account given proper input parameters.
+# Return true if account was created, false otherwise.
+# An accountType object can be obtained by:
+# AccountType.objects.get(pk=[1/2/3/4])
+# All other parameters are strings.
+def createAccount(firstName, lastName, username, password, email, phoneNumber, 
+                  accountType, accountBalance=100.00):
+    try:
+        person = Person()
+        person.firstName_text = firstName
+        person.lastName_text = lastName
+        person.username_text = username
+        person.password_text = password
+        person.email_email = email
+        person.phoneNumber_text = phoneNumber
+        person.accountBalance_decimal=accountBalance
+        person.accountType = accountType
+        person.save()
+        printDebug("Account for " + firstName + " " + lastName + " created successfully")
+        return True
+    except Exception as e:
+        printDebug("Failed to create account for " + firstName + " " + lastName)
+        printDebug(str(e))
+        return False
 
+
+def printDebug(message):
+    if printDebugMessages:
+        print("[DEBUG] " + str(message))
--- src/djangoserver_cs3450/dansbagels/tests.py
+++ src/djangoserver_cs3450/dansbagels/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from dansbagels.models import *
+from dansbagels.dbfunctions import *
 
-# Create your tests here.
+class OwnerAccountPageTestCase(TestCase):
+    def setUp(self):
+        # nothing needed for setup
+        pass
+
+    def test_create_account(self):
+        # set up the account parameters
+        firstName = "FN"
+        lastName = "LN"
+        username = "fn.ln"
+        password = "test-password"
+        email = "test@test.com"
+        phoneNumber = "123-456-7890"
+        accountType = AccountType.objects.get(pk=2)
+
+        # create the account
+        self.assertEqual(True,
+                createAccount(firstName, lastName, username, password, email, phoneNumber, accountType))
+
+        # make sure some things saved
+        self.assertEqual("fn.ln", Person.objects.get(firstName_text=firstName).username_text)
